167189c7fb106911ccd4495c493c56c2f6cbd9d9
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-03-11 10:09:12 +0100

    adding show modal functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+const showModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const showModalFunction = function(){
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < showModal.length; i++)
+  showModal[i].addEventListener('click' , showModalFunction);
+
+
+  
