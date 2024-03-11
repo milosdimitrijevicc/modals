3b5dc79da72700918ebd28ed3615304ffdc74498
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-03-11 10:13:18 +0100

    complete functionality implemented

--- script.js
+++ script.js
@@ -6,10 +6,20 @@ const overlay = document.querySelector('.overlay');
 const showModalFunction = function(){
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+};
+const closewModalFunction = function(){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 }
 
 for (let i = 0; i < showModal.length; i++)
   showModal[i].addEventListener('click' , showModalFunction);
 
-
+closeModal.addEventListener('click' , closewModalFunction);
+overlay.addEventListener('click' , closewModalFunction);
+document.addEventListener('keydown' , function(e){
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')){
+    closewModalFunction();
+  }
+})
   
